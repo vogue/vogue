1737280fd333595bd19a155e39a462f97cd3a7ed
Naturalhair Vogue <naturalhair.vogue@gmail.com>
2011-12-26 22:10:06 +0900

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vogue::Application.config.secret_token = 'd8efed0e956661be3aaf59b51a9f59ea765ca61e8d934449f850e99ed54ac675afff7268517162c7be833a51d673531f74d7decd305ce41f9b3e7fdd45d69a63'
